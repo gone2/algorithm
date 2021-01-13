fe8d446d2d55fad2d9257e86afa6f95d66e6a5b2
전지원 <jeonjiwon@jeonjiwon-ui-MacBookPro.local>
2021-01-13 17:37:15 +0900

    baekjoon 10872 -- 2021.01.13

---
+++ baekjoon_0113.java
@@ -0,0 +1,21 @@
+// 백준 재귀함수 10872번
+
+import java.io.*;
+
+public class baekjoon_0113 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        if(N < 0 || N > 12) return;
+        
+        int num = factorial(N);
+        System.out.println(num);
+    }
+
+    public static int factorial(int N) {
+        if(N <= 1) return 1;
+
+        return N * factorial(N - 1);
+    }
+}
