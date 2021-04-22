8d3473ec8f23668a76682a3bed6945e0ac4e672f
전지원 <jeonjiwon@jeonjiwon-ui-MacBookPro.local>
2021-04-22 16:04:12 +0900

    print.java success

---
+++ print.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class print {
+    public static void main(String[] args) {
+        int[] priorities = {1, 1, 9, 1, 1, 1};  // {2, 1, 3, 2}
+        int location = 0;                       // 2
+
+        solution(priorities, location);
+    }
+
+    public static int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int priority : priorities) {
+            pq.offer(priority);
+        }
+
+        while(!pq.isEmpty()) {
+            for(int i = 0; i < priorities.length; i++) {
+                if(pq.peek() == priorities[i]) {
+                    if(location == i) {
+                        return answer;
+                    }
+                    answer++;
+                    pq.poll();
+                }
+            }
+            break;
+        }
+        System.out.println(answer);
+
+
+        return answer;
+    }
+}
